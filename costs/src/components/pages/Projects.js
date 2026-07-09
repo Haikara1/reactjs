3a7907f89645d04c4b8399fae94208a119171335
Daniel Lima <danisfilholima@gmail.com>
2026-07-09 16:41:13 -0300

    Fazendo o botão de excluir card funcionar em react

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -15,6 +15,8 @@ import ProjectCard from "../project/ProjectCard"
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
+    // const [projectUpdate, setProjectUpdate] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -39,6 +41,36 @@ function Projects() {
         }, 300)
     }, [])
 
+    function removeProject(id) {
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+    }
+
+    // function updateProject(id) {
+
+    //     fetch(`http://localhost:5000/projects/${id}`, {
+    //         method: 'UPDATE',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //     }).then(resp => resp.json())
+    //     .then(data => {
+    //         setProjectUpdate(projects.filter((project) => project.id !==id))
+    //     })
+    //     .catch(err => console.log(err))
+
+    // }
+
     return(
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -46,6 +78,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message msg={message} type="success"  />}
+            {projectMessage && <Message msg={projectMessage} type="success"  />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -55,6 +88,7 @@ function Projects() {
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />))}
                         {!removeLoading && <Loading />}
                         {removeLoading && projects.length === 0 && (
