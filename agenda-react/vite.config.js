e76392ac2ebc0f2dd20978a49aad1c7179c4abe6
Daniel Lima <danisfilholima@gmail.com>
2026-08-04 16:24:38 -0300

    Corrige configuração PWA para o Vercel

--- agenda-react/vite.config.js
+++ agenda-react/vite.config.js
@@ -5,67 +5,100 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 
-    base: "/reactjs/",
+    base: "/",
+
 
     plugins: [
 
         react(),
 
+
         VitePWA({
 
             registerType: "autoUpdate",
 
+
             devOptions: {
+
                 enabled: true
+
             },
+
 
             manifest: {
 
-              name: "Agenda React",
 
-              short_name: "Agenda",
+                name: "Agenda React",
 
-              description:
-                  "Aplicativo de gerenciamento de lembretes",
 
-              theme_color: "#111827",
+                short_name: "Agenda",
 
-              background_color: "#ffffff",
 
-              display: "standalone",
+                description:
+                    "Aplicativo de gerenciamento de lembretes",
 
-              orientation: "portrait",
 
-              lang: "pt-BR",
+                theme_color: "#111827",
 
-              start_url: "/reactjs",
 
-              scope: "/reactjs",
+                background_color: "#ffffff",
 
-              icons: [
 
-                  {
-                      src: "icon-192.png",
-                      sizes: "192x192",
-                      type: "image/png",
-                      purpose: "any"
-                  },
+                display: "standalone",
 
-                  {
-                      src: "icon-512.png",
-                      sizes: "512x512",
-                      type: "image/png",
-                      purpose: "any"
-                  },
 
-                  {
-                      src: "icon-512.png",
-                      sizes: "512x512",
-                      type: "image/png",
-                      purpose: "maskable"
-                  }
+                orientation: "portrait",
 
-              ]
+
+                lang: "pt-BR",
+
+
+                start_url: "/",
+
+
+                scope: "/",
+
+
+                icons: [
+
+
+                    {
+                        src: "/icon-192.png",
+
+                        sizes: "192x192",
+
+                        type: "image/png",
+
+                        purpose: "any"
+
+                    },
+
+
+                    {
+                        src: "/icon-512.png",
+
+                        sizes: "512x512",
+
+                        type: "image/png",
+
+                        purpose: "any"
+
+                    },
+
+
+                    {
+                        src: "/icon-512.png",
+
+                        sizes: "512x512",
+
+                        type: "image/png",
+
+                        purpose: "maskable"
+
+                    }
+
+
+                ]
 
             }
 
